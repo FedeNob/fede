322809a6c73fc2b65f34691b3eddf59958b6e5ba
Fede <federica.nobili13@gmail.com>
2024-05-04 13:31:40 +0200

    aggiunte che palle

--- main.cpp
+++ main.cpp
@@ -8,9 +8,14 @@ void ciaoKappa()
 {
     std::cout << "Ciao Kappa\n";
 }
+void chePalle()
+{
+    std::cout << "Che palle\n";
+}
 
 int main()
 {
     ciaoFede();
     ciaoKappa();
+    chePalle();
 }
